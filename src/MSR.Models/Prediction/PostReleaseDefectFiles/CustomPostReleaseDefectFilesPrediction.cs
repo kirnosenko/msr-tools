f379c14d3e1a3935bf2ff946a339be6c9a3ea395
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2011-03-23 17:48:32 +0300

    custom prediction model

---
+++ src/MSR.Models/Prediction/PostReleaseDefectFiles/CustomPostReleaseDefectFilesPrediction.cs
@@ -0,0 +1,53 @@
+/*
+ * MSR Tools - tools for mining software repositories
+ * 
+ * Copyright (C) 2011  Semyon Kirnosenko
+ */
+
+using System;
+
+using MSR.Data;
+using MSR.Models.Prediction.Predictors;
+
+namespace MSR.Models.Prediction.PostReleaseDefectFiles
+{
+	public class CustomPostReleaseDefectFilesPrediction : PostReleaseDefectFilesPrediction
+	{
+		public CustomPostReleaseDefectFilesPrediction()
+		{
+			Title = "Custom model";
+		}
+		public bool AddedLoc
+		{
+			set { if (value) this.AddAddedLocInFilesInCommitsPredictor(); }
+		}
+		public bool DefectCodeDensity
+		{
+			set { if (value) this.AddDefectCodeDensityForCodeInCommitsInFilesPredictor(); }
+		}
+		public bool DefectDensity
+		{
+			set { if (value) this.AddDefectDensityForCodeInCommitsInFilesPredictor(); }
+		}
+		public bool DeletedLoc
+		{
+			set { if (value) this.AddDeletedLocInFilesInCommitsPredictor(); }
+		}
+		public bool FilesTouchCount
+		{
+			set { if (value) this.AddFilesTouchCountInCommitsPredictor(); }
+		}
+		public bool NumberOfBugs
+		{
+			set { if (value) this.AddNumberOfBugsTouchFilesInCommitsFixedTillRevisionPredictor(); }
+		}
+		public bool TotalLoc
+		{
+			set { if (value) this.AddTotalLocInFilesTillRevisionPredictor(); }
+		}
+		public bool TraditionalDefectDensity
+		{
+			set { if (value) this.AddTraditionalDefectDensityForCodeInCommitsInFilesPredictor(); }
+		}
+	}
+}
