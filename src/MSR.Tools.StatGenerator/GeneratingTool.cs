17278996061c404e725001a83a6b0a30ed5bd859
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2011-04-02 13:40:37 +0400

    blame command for statistics tool

--- src/MSR.Tools.StatGenerator/GeneratingTool.cs
+++ src/MSR.Tools.StatGenerator/GeneratingTool.cs
@@ -6,6 +6,12 @@
 
 using System;
 using System.Diagnostics;
+using System.Linq;
+
+using MSR.Data;
+using MSR.Data.Entities;
+using MSR.Data.Entities.DSL.Selection;
+using MSR.Data.Entities.DSL.Selection.Metrics;
 
 namespace MSR.Tools.StatGenerator
 {
@@ -34,6 +40,56 @@ namespace MSR.Tools.StatGenerator
 					builder.TargetDir = templateDir;
 				}
 				builder.GenerateStat(data);
+			}
+		}
+		public void ShowBlame(string targetDir, string targetPath)
+		{
+			using (ConsoleTimeLogger.Start("getting blame time"))
+			using (var s = data.OpenSession())
+			{
+				Func<ProjectFileSelectionExpression,ProjectFileSelectionExpression> fileSelector = e =>
+				{
+					return targetDir != null ?
+						e.InDirectory(targetDir)
+						:
+						e.PathIs(targetPath);
+				};	
+			
+				var authors = 
+					(
+						from cb in s.SelectionDSL()
+							.Files().Reselect(fileSelector)
+							.Modifications().InFiles()
+							.CodeBlocks().InModifications().Added()
+						join c in s.Repository<Commit>() on cb.AddedInitiallyInCommitID equals c.ID
+						select c.Author
+					).Distinct().OrderBy(x => x).ToList();
+				
+				int maxAuthorLen = authors.Select(x => x.Length).Max();
+				if (maxAuthorLen < 6)
+				{
+					maxAuthorLen = 6;
+				}
+				string format = "{0,X}{1,15}{2,15}".Replace("X", maxAuthorLen.ToString());
+				string line = "-".Repeat(maxAuthorLen + 15 + 15);
+				Console.WriteLine(format, "Author", "Added LOC", "Current LOC");
+				Console.WriteLine(line);
+				foreach (var author in authors)
+				{
+					var code = s.SelectionDSL()
+						.Commits().AuthorIs(author)
+						.Files().Reselect(fileSelector)
+						.Modifications().InFiles()
+						.CodeBlocks().InModifications().AddedInitiallyInCommits().Fixed();
+					
+					double addedLoc = code.CalculateLOC();
+					Console.WriteLine(format,
+						author,
+						addedLoc,
+						addedLoc + code.ModifiedBy().CalculateLOC()
+					);
+				}
+				Console.WriteLine(line);
 			}
 		}
 	}
--- src/MSR.Tools.StatGenerator/Program.cs
+++ src/MSR.Tools.StatGenerator/Program.cs
@@ -17,6 +17,7 @@ namespace MSR.Tools.StatGenerator
 			string targetDir = null;
 			string outputDir = null;
 			string templateDir = null;
+			string targetPath = null;
 			
 			try
 			{
@@ -36,6 +37,9 @@ namespace MSR.Tools.StatGenerator
 						case "-t":
 							templateDir = args[++i];
 							break;
+						case "-p":
+							targetPath = args[++i];
+							break;
 						default:
 							break;
 					}
@@ -44,12 +48,15 @@ namespace MSR.Tools.StatGenerator
 			}
 			catch
 			{
-				Console.WriteLine("usage: MSR.Tools.Calculator CONFIG_FILE_NAME COMMAND [ARGS]");
+				Console.WriteLine("usage: MSR.Tools.StatGenerator CONFIG_FILE_NAME COMMAND [ARGS]");
 				Console.WriteLine("Commands:");
 				Console.WriteLine("  stat		create stat using templates");
 				Console.WriteLine("    -d DIR	directory to generate statistics for");
 				Console.WriteLine("    -o DIR	path to output directory");
 				Console.WriteLine("    -t DIR	relative path to templates directory");
+				Console.WriteLine("  blame		show blame for a path");
+				Console.WriteLine("    -d DIR	show blame for the directory");
+				Console.WriteLine("    -p PATH	show blame for the file");
 				
 				return;
 			}
@@ -68,6 +75,9 @@ namespace MSR.Tools.StatGenerator
 			{
 				case "stat":
 					generator.GenerateStat(targetDir, outputDir, templateDir);
+					break;
+				case "blame":
+					generator.ShowBlame(targetDir, targetPath);
 					break;
 				default:
 					Console.WriteLine("Unknown command {0}", cmd);
