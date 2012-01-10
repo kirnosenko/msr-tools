35654b91359ae9b205e72d4831ffb62a02dc3a50
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2012-01-10 12:57:55 +0300

    version 0.3.3

--- src/MSR/Properties/SharedAssemblyInfo.cs
+++ src/MSR/Properties/SharedAssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MSR Tools")]
-[assembly: AssemblyCopyright("Copyright © Semyon Kirnosenko 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © Semyon Kirnosenko 2010-2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
+[assembly: AssemblyVersion("0.3.3.0")]
+[assembly: AssemblyFileVersion("0.3.3.0")]
