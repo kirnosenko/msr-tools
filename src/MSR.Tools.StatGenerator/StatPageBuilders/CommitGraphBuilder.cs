d323abe8b55c0e5d4dece86f92d40b4777392e37
Artem Makayda <artem.methods@gmail.com>
2013-06-02 20:49:15 +0400

    More beautyx-axis on graphs, and Commit Graph added at reports

---
+++ src/MSR.Tools.StatGenerator/StatPageBuilders/CommitGraphBuilder.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * MSR Tools - tools for mining software repositories
+ * 
+ * Graduate work by Artem Makayda
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MSR.Data;
+using MSR.Data.Entities;
+using MSR.Data.Entities.DSL.Selection;
+using MSR.Data.Entities.DSL.Selection.Metrics;
+
+namespace MSR.Tools.StatGenerator.StatPageBuilders
+{
+    public class CommitGraphBuilder : StatPageBuilder
+    {
+        public CommitGraphBuilder()
+        {
+            PageName = "Commit Graph";
+            PageTemplate = "commitgraph.html";
+        }
+        public override IDictionary<string, object> BuildData(IRepositoryResolver repositories)
+        {
+            Dictionary<string, object> result = new Dictionary<string, object>();
+
+			var authors = repositories.Repository<Commit>()
+				.Select(x => x.Author)
+				.Distinct().ToList();
+
+            var codeByAuthor = (from author in authors select new
+			{
+                Name = author,
+				AddedCode = repositories.SelectionDSL()
+					.Commits().AuthorIs(author)
+					.Files().InDirectory(TargetDir)
+					.Modifications().InFiles()
+					.CodeBlocks().InModifications().AddedInitiallyInCommits()
+					.Fixed(),
+            }).ToList();
+
+            var statByAuthor =
+                from a in codeByAuthor
+                let authorCommits = a.AddedCode.Commits().Again().Count()
+                let authorFixes = a.AddedCode.Commits().Again().AreBugFixes().Count()
+                let authorRefactorings = a.AddedCode.Commits().Again().AreRefactorings().Count()
+            select new
+                {
+                    name = a.Name,
+                    fixes = (double) authorFixes / authorCommits * 100,
+                    refactorings = (double) authorRefactorings / authorCommits * 100
+                };
+
+            result.Add("authors", statByAuthor.OrderBy(x => x.name).ToArray());
+
+            return result;
+        }
+    }
+}
